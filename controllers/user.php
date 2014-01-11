3ab2fe8987c014480b187764cce3800d26b47595
Bhavya Shaktawat <bhavya@readybytes.in>
2014-01-11 13:55:24 +0530

    maintain session on user login and destroy it on logout added getId, clear function in session base and updated destroy function

--- controllers/user.php
+++ controllers/user.php
@@ -17,13 +17,28 @@ class FAControllerUser extends FAController
 
 		//verification passed
 		//check user is blocked or not
-		$verified = FAHelperUser::verifyCredentials($user['username'], $user['password'], FAHelperUtils::isEmailAddress($user['username'])); 
+		$isEmail  = FAHelperUtils::isEmailAddress($user['username']);
+		$verified = FAHelperUser::verifyCredentials($user['username'], $user['password'], $isEmail); 
 		if( $verified === true 
 						&&  FAHelperUser::isBlock($user['username']) === false){
 
 			
 			//TODO:redirection
 			
+			$find = array('username'=>$user['username']);
+			if ($isEmail){
+				$find = array('email'=>$user['username']);
+			}
+			
+			$model = $this->getModel();
+			$user_record = $model->findOne($find);
+			
+			//set user-id in session
+			$session = FASession::getInstance();
+			
+		
+			$user_id = (string) new MongoId($user_record['_id']); 
+			$session->set('user_id', $user_id);
 		}
 		else
 		{
@@ -39,5 +54,14 @@ class FAControllerUser extends FAController
 
 		$model 		= $this->getModel();
 		$model->register($user_data);
+	}
+	
+	public function logout()
+	{
+		// TODO : will understand it later 
+		session_destroy();
+
+		//does session checking is required to make sure whether any user is logged-in or not???
+		//TODO : redirect user to some page after logout
 	}
 }
